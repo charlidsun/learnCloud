fd42ba799f3cd873d124722fccc2beafbbe907c1
charlidsun <sunjingge@outlook.com>
2018-09-25 17:40:58 +0800

    dddd

--- configServer/src/main/java/com/sun/configServer/ConfigServerApplication.java
+++ configServer/src/main/java/com/sun/configServer/ConfigServerApplication.java
@@ -2,8 +2,14 @@ package com.sun.configServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+//开始配置中心
+@EnableConfigServer
+@EnableEurekaServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
